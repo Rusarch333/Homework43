cfc796b235483b20a24dbb41baf97be67a63cabd
Lite <rusarch333@gmail.com>
2024-05-09 17:23:56 +0300

    Fix name checkers in Figure3D

--- js/Figure3D.js
+++ js/Figure3D.js
@@ -25,10 +25,12 @@ class Figure3D {
       throw new Error('You can`t create instance of abstract class Figure3D!');
     }
 
+    name = name.trim(); // Позбавляємось зайвих пробілів та рядків типу " ", "  "
+
     if (typeof name !== "string") {
       throw new TypeError("Name of Figure3D class instance is not a string!");
     }
-    if (name === false) {
+    if (Boolean(name) === false) {
       throw new Error("Name of Figure3D class instance can't be empty!");
     }
 
