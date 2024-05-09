cd83b8026e31e3eec52355921a6e1258c5c0cbca
Lite <rusarch333@gmail.com>
2024-05-09 14:56:16 +0300

    Create Figeure3D

--- js/Figeure3D.js
+++ js/Figeure3D.js
@@ -1,1 +1,29 @@
 'user strict';
+
+// Створити абстрактний клас Figeure3D з властивостю ім'я (string не пуста) і методом обчислити об'єм.
+/**
+ * Клас, який реалізує 3D фігуру
+ */
+class Figeure3D {
+  constructor (name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+  set name(value) {
+    // ?????????????????????? ЗАВДАВАТИ ВСЕ ТАКИ СЕТ ????
+    if (typeof value !== 'string') {
+      throw new TypeError("Name is not a string!");
+    }
+    if (value === false) {
+      // ???????????????????????????
+      throw new Error("Name can't be empty!");
+    }
+    this._name = value;
+  }
+  getValume() {
+    throw Error("Can't call getValume() method from Figeure3D abstract class!");
+  }
+}
